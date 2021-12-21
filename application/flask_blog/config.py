472b50b5d0ec178e7362d3784057ff4ba7e83952
bambriz <beambriz2017@gmail.com>
2021-12-21 09:20:07 +0000

    added database, unfortunately, flask-script is broken and I will not be using it

--- application/flask_blog/config.py
+++ application/flask_blog/config.py
@@ -1,6 +1,9 @@
 #flask_blog/config.pow
 import os
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG = True
 USERNAME = os.getenv("usrnme")
 PASSWORD = os.getenv("psswrd")
 SECRET_KEY = os.getenv("secretKey")
+DFLIMGLNK = 'https://www.pngitem.com/pimgs/m/84-844866_face-nose-black-facial-expression-person-smile-emotion.png'
---
+++ application/flask_blog/models/entries.py
@@ -0,0 +1,20 @@
+from flask_blog import db
+from datetime import datetime
+DFLIMGLNK = 'https://www.pngitem.com/pimgs/m/84-844866_face-nose-black-facial-expression-person-smile-emotion.png'
+
+class Entry(db.model):
+	__tablename__ = 'entries'
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(50), unique=True)
+	imglnk = db.Column(db.String(100))
+	text = db.Column(db.Text)
+	created_at = db.Column(db.DateTime)
+
+	def __init__(self, title=None, imglnk=DFLIMGLNK, text=None):
+		self.title = title
+		self.imglnk = imglnk
+		self.text = text
+		self.created_at = datetime.utcnow
+
+	def __repr__(self):
+		return '<Entry id:{} title:{} imglnk:{} text:{}'.format(self.id, self.title, self.imglnk, self.text)
